ee11a0bf4667aa54fffbfebdc7db0e2ebf093510
quoctrieu2309 <trieunvph30369@fpt.edu.vn>
2024-01-07 14:57:10 +0700

    Feat: Complete create Size

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Role\RoleController;
+use App\Http\Controllers\Size\SizeController;
 use App\Models\Role;
 use Illuminate\Support\Facades\Route;
 
@@ -21,14 +22,22 @@ Route::group(['prefix'=> 'shoes-store'], function (){
       })->name('home');
 });
 
-Route::group(['prefix'=> 'admins'], function (){
+Route::group(['prefix'=> 'admin'], function (){
     Route::get('/dashboard',function(){
         return view('admins.layouts.master');
     })->name('dashboard');
-    Route::group(['prefix'=> 'roles'], function (){
+    Route::group(['prefix'=> 'role'], function (){
         Route::get('/',[RoleController::class,'index'])->name('roles.index');
         Route::post('/toggle-status', [RoleController::class,'toggleStatus'])->name('toggle.status');
         Route::get('/create',[RoleController::class,'create'])->name('roles.create');
         Route::post('/store',[RoleController::class,'store'])->name('roles.store');
+    });
+    Route::group(['prefix'=> 'size'], function (){
+        Route::get('/',[SizeController::class,'index'])->name('sizes.index');
+        Route::get('/create',[SizeController::class,'create'])->name('sizes.create');
+        Route::post('/store',[SizeController::class,'store'])->name('sizes.store');
+        Route::get('/edit/{id}',[SizeController::class,'edit'])->name('sizes.edit');
+        Route::put('/update/{id}',[SizeController::class,'update'])->name('sizes.update');
+        Route::delete('/delete/{id}',[SizeController::class,'destroy'])->name('sizes.destroy');
     });
 });
